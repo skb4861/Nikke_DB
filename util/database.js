b4b4e48b8d66acd2c076dc2cc9c5483a3de2191f
skb4861 <skb4861@gmail.com>
2024-03-09 01:34:38 +0900

    5.94 commit
    5.94

--- util/database.js
+++ util/database.js
@@ -1,14 +1,14 @@
 import { MongoClient } from 'mongodb'
-const url = 'mongodb+srv://admin:skb113028@cluster0.euvqotd.mongodb.net/project1?retryWrites=true'
+const MONGODB_URI = 'mongodb+srv://vercel-admin-user:GUMX2UDya2HICLpF@cluster0.euvqotd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const options = { useNewUrlParser: true }
 let connectDB
 
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongo) {
-    global._mongo = new MongoClient(url, options).connect()
+    global._mongo = new MongoClient(MONGODB_URI, options).connect()
   }
   connectDB = global._mongo
 } else {
-  connectDB = new MongoClient(url, options).connect()
+  connectDB = new MongoClient(MONGODB_URI, options).connect()
 }
 export { connectDB }
