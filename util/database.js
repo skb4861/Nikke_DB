0f9ea71a67367a7f1fbcbbcf4c976615ae385697
skb4861 <skb4861@gmail.com>
2024-03-09 01:28:35 +0900

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,14 +1,14 @@
 import { MongoClient } from 'mongodb'
-const MONGODB_URI = 'mongodb+srv://admin:skb113028@cluster0.euvqotd.mongodb.net/project1?retryWrites=true'
+const url = 'mongodb+srv://admin:skb113028@cluster0.euvqotd.mongodb.net/project1?retryWrites=true'
 const options = { useNewUrlParser: true }
 let connectDB
 
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongo) {
-    global._mongo = new MongoClient(MONGODB_URI, options).connect()
+    global._mongo = new MongoClient(url, options).connect()
   }
   connectDB = global._mongo
 } else {
-  connectDB = new MongoClient(MONGODB_URI, options).connect()
+  connectDB = new MongoClient(url, options).connect()
 }
 export { connectDB }
